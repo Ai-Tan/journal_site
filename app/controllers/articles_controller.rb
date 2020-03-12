83467c24ba5c6e0d11b661a3655ac4bc08f40945
tantan <tantant5656@gmail.com>
2020-03-12 14:00:14 +0900

    名前付きのパス、削除・編集追加、修正いたしました

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
+  
   def index
     @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -15,14 +16,25 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    redirect_to "/articles"
+    redirect_to articles_path
   end
 
-  # def destroy
-  #   @article = Article.find(params[:id])
-  #   @article.destroy
-  #   redirect_to articles_path
-  # end
+  def edit
+    @article = Article.find(params[:id])
+    
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
 
